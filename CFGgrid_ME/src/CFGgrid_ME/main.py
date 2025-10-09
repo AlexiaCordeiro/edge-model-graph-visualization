45d0dd8d8c4f8df515f4ac1c98931d7590e73da0
AlexiaCordeiro <alexiacordeiro@tutanota.com>
2025-10-09 14:42:18 -0300

    Added edges

--- CFGgrid_ME/src/CFGgrid_ME/main.py
+++ CFGgrid_ME/src/CFGgrid_ME/main.py
@@ -1,6 +1,7 @@
 from model_explorer import Adapter, AdapterMetadata, ModelExplorerGraphs, graph_builder
 from typing import Dict, List
 import re
+from pathlib import Path
 
 
 class CFGgridME(Adapter):
@@ -15,6 +16,7 @@ class CFGgridME(Adapter):
     def convert(self, model_path: str, settings: Dict) -> ModelExplorerGraphs:
         try:
             with open(model_path, "r") as f:
+                file_name = Path(model_path).stem
                 cfggrind_model = [line.strip() for line in f if line.strip()]
         except Exception as e:
             print(f"Error reading file: {e}")
@@ -23,32 +25,71 @@ class CFGgridME(Adapter):
         if not cfggrind_model:
             return {"graphs": []}
 
-        graph = graph_builder.Graph(id="cfg_graph")
+        graph = graph_builder.Graph(id=file_name)
         graphs = [graph]
         nodes = []
-
-        current_namespace = "default"
-
+        edges_connection = {}
+        edges = []
+        layers = "default"
         for line in cfggrind_model:
             if "cfg" in line:
                 layers = re.findall(r"cfg\s+(\S+)", line)
+            else:
+                edges = self.get_edges(line)
             op_match = re.match(r"^(?:\S+\s+){2}(\S+)", line)
+
             if not op_match:
                 continue
-
             op_name = op_match.group(1)
+
             if op_name == '"signal::main(11)"':
                 continue
 
-            clean_op_name = op_name.strip('"')
+            if "cfg" not in line:
+                edges_connection[op_name] = edges
+            else:
+                edges_connection[op_name] = "None"
 
-            node_id = op_name
-            node = graph_builder.GraphNode(
-                id=node_id,
-                label=clean_op_name,
-                namespace=str(layers[0]),
-            )
-            nodes.append(node)
+            self.create_node(op_name, layers[0], nodes)
 
         graph.nodes.extend(nodes)
+        for node in graph.nodes:
+            self.create_edges(node, edges_connection, graph)
         return {"graphs": graphs}
+
+    def create_node(self, op_name, layers, nodes):
+        node = graph_builder.GraphNode(
+            id=op_name,
+            label=op_name.strip('"'),
+            namespace=str(layers),
+        )
+        nodes.append(node)
+
+        return nodes
+
+    def create_edges(self, node, edges_connection, graph):
+        node_edges = edges_connection.get(node.id, [])
+        for edge in node_edges:
+
+            target_node = next((n for n in graph.nodes if n.id == edge), None)
+            if target_node:
+                target_node.incomingEdges.append(
+                    graph_builder.IncomingEdge(
+                        sourceNodeId=node.id,
+                        sourceNodeOutputId=node.id,
+                        targetNodeInputId=target_node.id,
+                    )
+                )
+        return node
+
+    def get_edges(self, line):
+        op_match = re.findall(r"(?<=\[)(.*?)(?=\])", line)
+        edges = []
+        values = op_match[-1].split(" ")
+
+        for edge in values:
+            edge = edge.replace("[", "").replace("]", "").split(":")
+            edges.append(edge[0])
+        
+        return edges
+
